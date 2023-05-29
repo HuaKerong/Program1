73edb9f180214929771d9c469425946e09c9cfef
Huakerong <engineerhkr@163.com>
2023-05-29 18:44:27 +0800

    Initial Commit

---
+++ Program1/Program1App.swift
@@ -0,0 +1,17 @@
+//
+//  Program1App.swift
+//  Program1
+//
+//  Created by 123456 on 2023/5/29.
+//
+
+import SwiftUI
+
+@main
+struct Program1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
